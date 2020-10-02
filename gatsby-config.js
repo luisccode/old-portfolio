825190941b8395d7e3960d7083f026c35bd2aaf8
Luis Cortes <luisac101299@gmail.com>
2020-10-02 07:42:20 -0400

    updated gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,13 +34,7 @@ module.exports = {
       options: {
         apiURL: process.env.API_URL || "http://localhost:1337",
         queryLimit: 1000,
-        contentTypes: [
-          "sections",
-          "services",
-          "projects",
-          "testimonials",
-          "articles",
-        ],
+        contentTypes: ["sections", "services", "projects", "articles"],
       },
     },
     `gatsby-plugin-styled-components`,
@@ -57,20 +51,18 @@ module.exports = {
       },
     },
     `gatsby-plugin-mdx`,
-    //{
-    //resolve: `gatsby-plugin-manifest`,
-    //options: {
-    //name: `gatsby-starter-default`,
-    //short_name: `starter`,
-    //start_url: `/`,
-    //background_color: `#663399`,
-    //theme_color: `#663399`,
-    //display: `minimal-ui`,
-    //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //},
-    //},
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Luisccode Portfolio`,
+        short_name: `Luisccode Portfolio`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
+        display: `minimal-ui`,
+        icon: `src/images/male-technologist.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 };
